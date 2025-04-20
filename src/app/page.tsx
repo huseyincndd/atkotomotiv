7abbc643b8f0b7c4cf90f541b1dee7672445357a
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 <cuneydiogluhuseyin1@gmail.com>
2025-04-20 12:25:48 +0300

    mobile problem2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import Link from 'next/link';
 import SketchfabSeat from '@/components/SketchfabSeat';
-import { useState, useRef } from 'react';
 
 export default function Home() {
   return (
